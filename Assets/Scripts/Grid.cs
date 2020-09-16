e8a31d96c973f1a4412b3565c4f19866ccf3f1c4
Kryštof Novosad <novosadkry@seznam.cz>
2020-09-16 21:20:07 +0200

    Init commit

---
+++ Assets/Scripts/Grid.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Grid : MonoBehaviour
+{
+    MeshFilter meshFilter;
+    MeshRenderer meshRenderer;
+    MeshCollider meshCollider;
+
+    GridCell cell = new GridCell();
+
+    public List<float> values;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        meshFilter = GetComponent<MeshFilter>();
+        meshRenderer = GetComponent<MeshRenderer>();
+        meshCollider = GetComponent<MeshCollider>();
+
+        for (int i = 0; i < values.Count; i++)
+            cell.Values[i] = values[i];
+
+        Mesh mesh = cell.ConstructMesh(0.5f);
+        meshFilter.mesh = mesh;
+        meshCollider.sharedMesh = mesh;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.G))
+        {
+            for (int i = 0; i < values.Count; i++)
+                cell.Values[i] = values[i];
+
+            Mesh mesh = cell.ConstructMesh(0.5f);
+            meshFilter.mesh = mesh;
+            meshCollider.sharedMesh = mesh;
+        }
+    }
+}
---
+++ Assets/Scripts/GridCell.cs
@@ -0,0 +1,93 @@
+﻿using System.Linq;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridCell
+{
+    public struct Edge
+    {
+        public Edge(Vector3 a, Vector3 b)
+        {
+            A = a;
+            B = b;
+        }
+
+        public Vector3 A { get; }
+        public Vector3 B { get; }
+
+        public Vector3 Midpoint { get => (A + B) / 2; }
+    }
+
+    public static Vector3[] vertices =
+    {
+        new Vector3(0, 0, 0),
+        new Vector3(1, 0, 0),
+        new Vector3(1, 1, 0),
+        new Vector3(0, 1, 0),
+        new Vector3(0, 0, 1),
+        new Vector3(1, 0, 1),
+        new Vector3(1, 1, 1),
+        new Vector3(0, 1, 1),
+    };
+
+    public static Edge[] edges =
+    {
+        new Edge(vertices[0], vertices[1]),
+        new Edge(vertices[1], vertices[2]),
+        new Edge(vertices[2], vertices[3]),
+        new Edge(vertices[3], vertices[0]),
+        new Edge(vertices[4], vertices[5]),
+        new Edge(vertices[6], vertices[5]),
+        new Edge(vertices[7], vertices[6]),
+        new Edge(vertices[7], vertices[4]),
+        new Edge(vertices[0], vertices[4]),
+        new Edge(vertices[1], vertices[5]),
+        new Edge(vertices[6], vertices[2]),
+        new Edge(vertices[3], vertices[7]),
+    };
+
+    public float[] Values { get; } = new float[8];
+
+    public Mesh ConstructMesh(float surfaceLevel)
+    {
+        int cubeIndex = 0;
+        for (int i = 0; i < 8; i++)
+        {
+            if (Values[i] < surfaceLevel)
+                cubeIndex |= 1 << i;
+        }
+
+        List<Vector3> vertices = new List<Vector3>();
+        List<int> triangles = new List<int>();
+
+        int[] tri = Table.triangulation[cubeIndex];
+        int vertexCount = 0;
+
+        foreach (int edgeIndex in tri)
+        {
+            if (edgeIndex == -1)
+                continue;
+
+            Edge edge = edges[edgeIndex];
+            vertices.Add(edge.Midpoint);
+            triangles.Add(vertexCount++);
+        }
+
+        foreach (int edgeIndex in tri.Reverse())
+        {
+            if (edgeIndex == -1)
+                continue;
+
+            Edge edge = edges[edgeIndex];
+            vertices.Add(edge.Midpoint);
+            triangles.Add(vertexCount++);
+        }
+
+        Mesh mesh = new Mesh();
+        mesh.vertices = vertices.ToArray();
+        mesh.triangles = triangles.ToArray();
+        mesh.RecalculateNormals();
+
+        return mesh;
+    }
+}
